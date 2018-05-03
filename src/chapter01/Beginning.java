de3a3b176b403f14ea4708a75bb309eb368e04a9
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2018-05-03 20:13:17 +0300

    HeadFirstJava first commit

---
+++ src/chapter01/Beginning.java
@@ -0,0 +1,30 @@
+package chapter01;
+
+
+public class Beginning {
+    public static void main(String[] args) {
+        double d = Math.random();
+        double x = Math.random();
+
+        double a = d * x;
+
+        System.out.println(d);
+        System.out.println(x);
+        System.out.println("  ");
+        System.out.println(a);
+        System.out.println("  ");
+
+        int z = 1;
+
+        System.out.println("Перед началом цикла");
+
+        while (z < 4){
+            System.out.println("Внутри цикла");
+            System.out.println("z равно - " + z);
+
+            z++;
+        }
+        System.out.println("После окончания цикла");
+    }
+
+}
---
+++ src/chapter01/PhraseOMatic.java
@@ -0,0 +1,21 @@
+package chapter01;
+
+public class PhraseOMatic {
+    public static void main(String[] args) {
+        String[] wordListOne = {"круглосуточный","трех-звёздный","30000-футовый","взаимный","умный","шесть"};
+        String[] wordListTwo = {"трудный","фирменный","сетевой","общий","совместный","ускоренный","распределенный"};
+        String[] wordListThree = {"обзор","легко","портал","талант","подход"};
+
+        int oneLength = wordListOne.length;
+        int twoLength = wordListTwo.length;
+        int threeLength = wordListThree.length;
+
+        int rand1 = (int) (Math.random() * oneLength);
+        int rand2 = (int) (Math.random() * twoLength);
+        int rand3 = (int) (Math.random() * threeLength);
+
+        String phrase = wordListOne[rand1] + " " + wordListTwo[rand2] + " " + wordListThree[rand3];
+
+        System.out.println("Всё, что вам нужно, - это " + phrase);
+    }
+}
