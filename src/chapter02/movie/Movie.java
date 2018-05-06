042dee1dfc245f7b02f2738093fb2b66ff4ed7a2
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2018-05-06 13:45:50 +0300

    HeadFirstJava Movie commit

--- src/chapter02/movie/Movie.java
+++ src/chapter02/movie/Movie.java
@@ -1,4 +1,11 @@
 package chapter02.movie;
 
 public class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void playIt() {
+        System.out.println("Запуск фильма.");
+    }
 }
---
+++ src/chapter02/movie/MovieTestDrive.java
@@ -0,0 +1,22 @@
+package chapter02.movie;
+
+public class MovieTestDrive {
+    public static void main(String[] args) {
+        Movie one = new Movie();
+        one.title = "How to lose";
+        one.genre = "Tragedy";
+        one.rating = -2;
+
+        Movie two = new Movie();
+        two.title = "Lost in Office";
+        two.genre = "Comedy";
+        two.rating = 5;
+        two.playIt();
+
+        Movie three = new Movie();
+        three.title = "Byte-club";
+        three.genre = "Tragedy but fun";
+        three.rating = 127;
+
+    }
+}
