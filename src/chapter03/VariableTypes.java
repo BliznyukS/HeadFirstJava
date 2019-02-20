b63564655c776fbbad9b5f59819144e9075dbe49
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2019-02-20 11:00:27 +0200

    VariableTypes

---
+++ src/chapter03/VariableTypes.java
@@ -0,0 +1,15 @@
+package chapter03;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+
+        char c = '1';
+        boolean t = true;
+        boolean f = false;
+        byte b = 127;
+        short s = 32767;
+        int i = 2147483647;
+        long l = 3656456799999999999L;
+
+    }
+}
