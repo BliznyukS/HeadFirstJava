8a5c9658458ad573b19e8891780d4a64338901e5
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2019-06-04 13:50:19 +0300

    chapter05

---
+++ src/chapter05/simpleGame/SimpleDotComTestDrive.java
@@ -0,0 +1,15 @@
+package chapter05.simpleGame;
+
+public class SimpleDotComTestDrive {
+    public static void main(String[] args) {
+
+        SimpleDotCom dot = new SimpleDotCom();
+
+        int[] locations = {2, 3, 4};
+        dot.setLocationCells(locations);
+
+        String userGuess = "1";
+        String result = dot.checkYourself(userGuess);
+    }
+}
+
