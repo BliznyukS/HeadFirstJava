bde537685b550a44bb5f32387e23af5096e0557c
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2019-08-30 19:31:45 +0300

    add MusicTest1 add Try\catch block

--- src/chapter11/MusicTest1.java
+++ src/chapter11/MusicTest1.java
@@ -1,13 +1,20 @@
 package chapter11;
 
 import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequencer;
 
 public class MusicTest1 {
 
-    public void play(){
-        Sequencer sequencer = MidiSystem.getSequencer();
-        System.out.println("мы получили синтезатор");
+    public void play() {
+
+        try {
+            Sequencer sequencer = MidiSystem.getSequencer();
+            System.out.println("мы получили синтезатор");
+        }
+        catch (MidiUnavailableException ex){
+            System.out.println("fail");
+        }
     }
 
     public static void main(String[] args) {
