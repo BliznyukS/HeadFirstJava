a45cbeddc40ccd3d0bb674bd6d63bf21b118ab01
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2019-09-04 21:02:05 +0300

    add Beatbox

---
+++ src/chapter13/BeatBox.java
@@ -0,0 +1,48 @@
+package chapter13;
+
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Track;
+import javax.swing.*;
+import java.util.ArrayList;
+
+public class BeatBox {
+
+    JPanel mainPanel;
+    ArrayList<JCheckBox> checBoxList;
+    Sequencer sequencer;
+    Sequence sequence;
+    Track track;
+    JFrame theFrame;
+
+    String[] instrumentNames = {
+            "Bass Drum",
+            "Closed Hi-Hat",
+            "Open Hi-Hat",
+            "Acoustic Snare",
+            "Crash Cymbal",
+            "Hand Clap",
+            "High Tom",
+            "High Bongo",
+            "Maracas",
+            "Whistle",
+            "Low Conga",
+            "Cowbell",
+            "Vibraslap",
+            "Low-mid Tom",
+            "High Agogo",
+            "Open Hi Conga"};
+
+    int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+
+    public static void main(String[] args) {
+        new BeatBox().buildGUI();
+    }
+
+    private void buildGUI() {
+
+
+    }
+
+
+}
