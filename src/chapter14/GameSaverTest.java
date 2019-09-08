eecf96e3047a94b4f35bb265be2f0bcd387e3413
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2019-09-08 12:54:53 +0300

    completed GameSaverTest abd GameCharacter

--- src/chapter14/GameSaverTest.java
+++ src/chapter14/GameSaverTest.java
@@ -1,9 +1,42 @@
 package chapter14;
 
+import java.io.*;
+
 public class GameSaverTest {
 
     public static void main(String[] args) {
 
-        GameCharacter one = new GameCharacter();
+        GameCharacter one = new GameCharacter(50, "Elf", new String[]{"Bow", "Sword", "Knuckles"});
+        GameCharacter two = new GameCharacter(200, "Troll", new String[]{"Hands", "Great Axe"});
+        GameCharacter three = new GameCharacter(120, "Wizard", new String[]{"Spells", "Invisibility"});
+
+        try {
+            ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream("Game.ser"));
+            os.writeObject(one);
+            os.writeObject(two);
+            os.writeObject(three);
+            os.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        one = null;
+        two = null;
+        three = null;
+
+        try {
+            ObjectInputStream is = new ObjectInputStream(new FileInputStream("Game.ser"));
+            GameCharacter oneRestore = (GameCharacter) is.readObject();
+            GameCharacter twoRestore = (GameCharacter) is.readObject();
+            GameCharacter threeRestore = (GameCharacter) is.readObject();
+
+            System.out.println("type of first character : " + oneRestore);
+            System.out.println("type of second character : " + twoRestore);
+            System.out.println("type of third character : " + threeRestore);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
