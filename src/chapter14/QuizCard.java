ea7d9899b9bcba19553365869c58ca69cce0e6d6
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2019-09-08 14:35:12 +0300

    add QuizCard, QuizCardBuilder

---
+++ src/chapter14/QuizCard.java
@@ -0,0 +1,16 @@
+package chapter14;
+
+public class QuizCard {
+
+    public QuizCard(String text, String text1) {
+
+    }
+
+    public int getQuestion() {
+        return 1;
+    }
+
+    public int getAnswer() {
+        return 1;
+    }
+}
