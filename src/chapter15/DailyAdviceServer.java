b73f1afd901e511264b294163707435521afdd1b
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2019-09-09 19:55:54 +0300

    add DailyAdviceServer

---
+++ src/chapter15/DailyAdviceServer.java
@@ -0,0 +1,41 @@
+package chapter15;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class DailyAdviceServer {
+
+    String adviceList[] = {"Eat less", "Buy new jeans", "Two words : not good", "Be honest with your boss, tell him the truth", "Change haircut"};
+
+    public void go() {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(4242);
+
+            while (true) {
+
+                Socket sock = serverSocket.accept();
+
+                PrintWriter writer = new PrintWriter(sock.getOutputStream());
+                String advice = getAdvice();
+                writer.println(advice);
+                writer.close();
+                System.out.println(advice);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private String getAdvice() {
+        int random = (int) (Math.random() * adviceList.length);
+        return adviceList[random];
+    }
+
+    public static void main(String[] args) {
+        DailyAdviceServer server = new DailyAdviceServer();
+        server.go();
+    }
+}
