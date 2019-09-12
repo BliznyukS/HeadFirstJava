281197bedd693cc0a9ac475e24460ac099e7c448
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2019-09-12 20:32:02 +0300

    edit RyanAndMonicaJob

--- src/chapter15/RyanAndMonicaJob.java
+++ src/chapter15/RyanAndMonicaJob.java
@@ -7,7 +7,7 @@ class BankAccount {
         return balance;
     }
 
-    public void withdraw() {
+    public void withdraw(int amount) {
         balance = balance - amount;
     }
 }
@@ -17,11 +17,48 @@ public class RyanAndMonicaJob implements Runnable {
     private BankAccount account = new BankAccount();
 
     public static void main(String[] args) {
+        RyanAndMonicaJob theJob = new RyanAndMonicaJob();
 
+        Thread one = new Thread(theJob);
+        Thread two = new Thread(theJob);
+
+        one.setName("Ryan");
+        two.setName("Monica");
+
+        one.start();
+        two.start();
     }
 
     @Override
     public void run() {
 
+        for (int x = 0; x < 10; x++) {
+            makeWithdrawal(10);
+
+            if (account.getBalance() < 0) {
+                System.out.println("Over Limmit !");
+            }
+
+        }
+
+    }
+
+    private void makeWithdrawal(int amount) {
+
+        if (account.getBalance() >= amount) {
+            System.out.println(Thread.currentThread().getName() + "Going to get money");
+
+            try {
+                System.out.println(Thread.currentThread().getName() + "Going to sleep");
+                Thread.sleep(500);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + "Wakeup");
+            account.withdraw(amount);
+            System.out.println(Thread.currentThread().getName() + "Finishing transaction");
+        } else {
+            System.out.println("Sorry for client - " + Thread.currentThread().getName() + " not enough money");
+        }
     }
 }
