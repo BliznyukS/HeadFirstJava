24e96cf786ed66d99ea1939351a25c044175af15
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2019-09-17 21:05:33 +0300

    add Jukebox6 with HashSet

---
+++ src/chapter16/Jukebox6.java
@@ -0,0 +1,68 @@
+package chapter16;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+
+public class Jukebox6 {
+
+    ArrayList<Song> songList = new ArrayList<Song>();
+
+    public static void main(String[] args) {
+        new Jukebox6().go();
+    }
+
+    class ArtistCompare implements Comparator<Song> {
+
+        @Override
+        public int compare(Song one, Song two) {
+            return one.getArtist().compareTo(two.getArtist());
+        }
+    }
+
+    class RatingCompare implements Comparator<Song> {
+
+        @Override
+        public int compare(Song one, Song two) {
+            return one.getRating().compareTo(two.getRating());
+        }
+    }
+
+    private void go() {
+        getSongs();
+        System.out.println("Before sort - " + songList);
+        Collections.sort(songList);
+        System.out.println("After sort - " + songList);
+
+        HashSet<Song> songSet = new HashSet<Song>();
+        songSet.addAll(songList);
+        System.out.println("Sort with HashSet - " + songSet);
+
+
+    }
+
+    void getSongs() {
+
+        try {
+            File file = new File("SongList.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    void addSong(String lineToParse) {
+        String[] tokens = lineToParse.split("/");
+        Song nextSong = new Song(tokens[0], tokens[1], tokens[2], tokens[3]);
+        songList.add(nextSong);
+    }
+}
