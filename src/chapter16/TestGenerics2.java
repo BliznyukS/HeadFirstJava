eebbe4327c6895287ea076e44895994da7e16684
Serhii Blyzniuk <bliznyuk.sergey@gmail.com>
2019-09-19 15:24:41 +0300

    edit TestGenerics2

--- src/chapter16/TestGenerics2.java
+++ src/chapter16/TestGenerics2.java
@@ -1,5 +1,7 @@
 package chapter16;
 
+import java.util.ArrayList;
+
 public class TestGenerics2 {
 
     public static void main(String[] args) {
@@ -7,6 +9,21 @@ public class TestGenerics2 {
     }
 
     private void go() {
+        ArrayList<Animal> animals = new ArrayList<Animal>();
 
+        animals.add(new Dog());
+        animals.add(new Cat());
+        animals.add(new Dog());
+
+        takeAnimals(animals);
     }
+
+    public void takeAnimals(ArrayList<Animal> animals) {
+
+        for (Animal a : animals) {
+            a.eat();
+        }
+    }
+
+
 }
